67c80f6638f5d3691d31323147d50966033904cd
QA-Exercise <104518021+QA-Exercise@users.noreply.github.com>
2022-04-27 12:30:30 -0400

    Update steps.js

--- features/step-definitions/steps.js
+++ features/step-definitions/steps.js
@@ -3,4 +3,5 @@ const SubmitFormPage = require('../pageobjects/submit.form.page');
 
 Given(/^I am on the demoqa page$/, async () => {
     await SubmitFormPage.open();
+    await browser.debug();
 });
