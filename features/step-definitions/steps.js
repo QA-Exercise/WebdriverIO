86c0d38c13724159c726604fdcdd42aa41536c46
QA-Exercise <104518021+QA-Exercise@users.noreply.github.com>
2022-04-27 12:28:52 -0400

    Update steps.js

--- features/step-definitions/steps.js
+++ features/step-definitions/steps.js
@@ -1,6 +1,6 @@
 const { Given, When, Then } = require('@wdio/cucumber-framework');
 const SubmitFormPage = require('../pageobjects/submit.form.page');
 
-Given(/^I am on the demoqa page$/, async (page) => {
+Given(/^I am on the demoqa page$/, async () => {
     await SubmitFormPage.open();
 });
